4e268b06671654168d5e2dd665be5ab31b8bda50
abrahamlundy <abraham.lundy.batasche@gmail.com>
2017-04-05 10:37:09 +0700

    versi google on location Change

--- app/src/main/java/com/example/avraham/mapfencing/MapsActivity.java
+++ app/src/main/java/com/example/avraham/mapfencing/MapsActivity.java
@@ -1,19 +1,31 @@
 package com.example.avraham.mapfencing;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     private GoogleMap mMap;
+    private Marker mPosKini;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +60,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(GedungIlkom).title("Marker di GIK"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(GedungIlkom));
 
+        LatLng posKini= new LatLng(-6.8601418,107.589889);
+        mPosKini= mMap.addMarker(new MarkerOptions().position(posKini).title("Posisi Sekarang"));
+
         LatLngBounds UPI= new LatLngBounds(new LatLng(-6.863273,107.587212), new LatLng(-6.858025,107.597839));
 
         //mengeset batas kamera ke UPI
@@ -55,6 +70,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         int heigth= getResources().getDisplayMetrics().heightPixels;
         int padding= (int)(width*0.12); //offset dari edges
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(UPI,width,heigth,padding));
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        AlertDialog ad = new AlertDialog.Builder(this).create();
+        ad.setMessage("Update Lokasi");
+        ad.show();
+        mPosKini.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
